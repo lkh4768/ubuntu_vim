a200a0a613b13bbcd6947856656e25da54869b31
lkh4768 <lkh5510@gmail.com>
2017-08-07 01:22:23 +0900

    add vim-go, YouCompleteMe

--- Dockerfile
+++ Dockerfile
@@ -2,22 +2,30 @@ FROM ubuntu
 
 RUN apt-get update && apt-get install -y --no-install-recommends \
 	build-essential \
-	vim \
+	cmake \
 	ctags \
 	git \
 	wget \
 	python \
-	golang-go \
+	python-dev \
+	python3-dev \
+	golang \
 	openssh-server \
+	mercurial \
+	clang \
+	libncurses5-dev \
+	libncursesw5-dev \
+	vim-nox \
 	&& rm -rf /var/lib/apt/lists/* \
 # config sshd
 	&& mkdir /var/run/sshd \
 	&& sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config \
 	&& echo "Ciphers chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com,aes256-cbc" >> /etc/ssh/sshd_config
 
-COPY files/vimrc_lite /
+COPY files/vimrc_ide /
 COPY files/distinguished.vim /
 COPY files/docker-entrypoint.sh /
+COPY files/ycm_extra_conf.py /
 
 ENTRYPOINT ["/docker-entrypoint.sh"]
 
--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -42,17 +42,57 @@ fi
 echo "$USER_NAME""'s password: ""$USER_PASS"
 echo "$USER_NAME:$USER_PASS" | chpasswd
 
-# install vim plugin to root
+# config env
+echo "------config env------"
 git config --global http.sslVerify false
+export GOPATH=$HOME/golang
+export PATH=$PATH:$GOPATH/bin
+
+# install vim version 8.0
+#echo "------install vim version 8.0------"
+#git clone https://github.com/vim/vim.git
+#cd vim
+#./configure \
+#	--enable-pythoninterp=yes \
+#	--with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+#	--enable-python3interp=yes \
+#	--with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu
+#cd src
+#make distclean;make;make install
+
+# install vim-bundle
+echo "------install vim-plugin------"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp /vimrc_lite ~/.vimrc
+cp /vimrc_ide ~/.vimrc
 mkdir ~/.vim/colors
 cp -f /distinguished.vim ~/.vim/colors
+
+# install vim-go
+echo "------install vim-go------"
+mkdir -p ~/.vim/autoload ~ 
+cd ~/.vim/autoload
+curl -LSso pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+git clone https://github.com/fatih/vim-go.git
+vim +GoInstallBinaries +qa
+
+# install vim-plugin
+echo "------install vim-plugin(YouCompleteMe)------"
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+~/.vim/bundle/YouCompleteMe/install.py --clang-completer --gocode-completer
+cp -f /ycm_extra_conf.py ~/.vim/
 vim +PluginInstall +qall +qa
 
-# cp vim plugin to USER_NAME
+# config bashrc 
 USER_HOME="/home/""$USER_NAME""/"
 echo "git config --global http.sslVerify false" >> "$USER_HOME"".bashrc"
+echo "GOPATH=\$HOME/golang" >> "$USER_HOME"".bashrc"
+echo "PATH=\$PATH:\$GOPATH/bin" >> "$USER_HOME"".bashrc"
+
+# cp vim plugin and go workspace to USER_NAME
+cp -r ~/golang $USER_HOME 
 cp -r ~/.vim $USER_HOME
 cp ~/.vimrc $USER_HOME
 chown -R $USER_NAME:$USER_NAME $USER_HOME
